18fdc32357e76ab1d98b1457ac928290a12148d5
aditya <adi.kgp@gmail.com>
2020-02-12 13:04:21 +0530

    included functinality of adding pagination for product list and creating account header and order history in account page

---
+++ pages/api/orders.js
@@ -0,0 +1,20 @@
+import connectDb from '../../utils/connectDb';
+import Order from '../../models/Order';
+import jwt from 'jsonwebtoken';
+
+connectDb();
+
+export default async (req, res) => {
+  try {
+    const {userId} = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    const orders = await Order.find({user: userId}).populate({
+      path:'products.product',
+      model: 'Product'
+    });
+    res.status(200).json({orders}); // array of orders
+  } catch(error) {
+    console.error(error);
+    res.status(403).send('Please login again');
+  }
+  
+}
--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,27 @@ import React from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
 import baseUrl from '../utils/baseUrl';
+import ProductPagination from '../components/Index/ProductPagination';
 
-function Home({products}) {
-  return <ProductList products = {products} />;
+function Home({products, totalPages}) {
+  return (
+    <>
+    <ProductList products = {products} />
+    <ProductPagination totalPages = {totalPages} />
+    </>
+  );
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async (ctx) => {
+  const page = ctx.query.page ? ctx.query.page : "1" ; // console.log(ctx)
+  const size = 9;
   // fetch data on server
+  const payload = { params: {page, size}}
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
+  const response = await axios.get(url,payload);
   //return response data as an object
   // this object will be merged with existing props
-  return {products: response.data};
+  return response.data;
 }
 
 export default Home;
