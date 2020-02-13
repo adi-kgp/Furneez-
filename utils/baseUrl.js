f9f8b6fb18f5b5da8f6899f338e1c3d2b3407b0b
aditya <adi.kgp@gmail.com>
2020-02-13 13:10:39 +0530

    nextjs app on heroku

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,4 +1,4 @@
 const baseUrl = process.env.NODE_ENV === 'production'
-  ? 'https://deployment-url.now.sh' : 'http://localhost:3000';
+  ? 'https://furneez.now.sh' : 'http://localhost:3000';
 
 export default baseUrl;  
