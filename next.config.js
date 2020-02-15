8094404f86ebb103c2471e41e8cb1592c4a120d9
aditya sharma <adi.kgp@gmail.com>
2020-02-15 09:19:22 +0530

    Update next.config.js
    hiding secret keys used in the app

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "mongodb+srv://AdityaSharma:adityasharma1991@furneez-rwzjf.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "adskfiulgpiuidioasdfyu",
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dwglildtm/image/upload",
-    STRIPE_SECRET_KEY: "sk_test_RFT4Bk4028yElNcNaWwpPST100aCy4OuFA"
+    MONGO_SRV: "hidden",
+    JWT_SECRET: "hidden",
+    CLOUDINARY_URL: "hidden",
+    STRIPE_SECRET_KEY: "hidden"
   }
 };
